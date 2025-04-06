c0bcfa5331faf70ea6bcaa82fcae9b076d4085f5
AnisHerdev <88375763+AnisHerdev@users.noreply.github.com>
2025-04-07 00:04:23 +0530

    Merged the automation

--- tkinterApp.py
+++ tkinterApp.py
@@ -73,8 +73,8 @@ class GUI:
         self.computerChoiceImg.image = static_image_tk  # Keep a reference to avoid garbage collection
         # self.btn = tk.Button(self.root, text="Computer", font=('Arial', 16), command=self.updateComputerChoice) # UpdatecomputerChoice manually
         # self.btn.pack()
-        self.btn2 = tk.Button(self.root, text="ScoreUpdate", font=('Arial', 16), command=self.updateScore) # UpdateScore manually
-        self.btn2.pack()
+        # self.btn2 = tk.Button(self.root, text="ScoreUpdate", font=('Arial', 16), command=self.updateScore) # UpdateScore manually
+        # self.btn2.pack()
         # OpenCV Video Capture
         self.cap = cv2.VideoCapture(0)  # Use 0 for webcam or replace with video file path
 
